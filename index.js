51b8b56efc91e2bc9ad5ebc999aafddd396d4586
Titus Mumo <titusmumo@gmail.com>
2022-08-30 23:41:40 +0300

    adding products to html

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const public_api_url = 'https://fakestoreapi.com/products';
 
-function initialize () {
-    getData()
-}
-
 function getData() {
-   const response = fetch (public_api_url)
+    fetch (public_api_url)
    .then (response => response.json())
-   .then (data => {
-    JSON.stringify(data, ['id', 'category', 'title', 'price'])
-   }); 
+   .then (products => {
+    products;
+    let data = JSON.stringify(products,['id', 'category', 'title', 'image', 'price']);
+    console.log(products.length);
+
+    document.querySelector('div').innerHTML = `
+    <h1 class ="title">products (${products.length}results)</h1>
+    ${products.map(function (product){
+        return `
+        <div class = item>
+        <img src = "${product.image}">
+        </div>
+        `
+    }).join('')}
+    <p>the place to place ${products.length}</p>
+    `
+}); 
 }
 
-
+getData()
