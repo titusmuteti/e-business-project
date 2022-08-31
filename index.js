78858efc8e228ffa166db46bcb530840266e98c4
Titus Mumo <titusmumo@gmail.com>
2022-08-31 14:43:55 +0300

    header

--- index.js
+++ index.js
@@ -14,23 +14,22 @@ function getData() {
 
     const divs = document.getElementById('items');
     //styling
-    divs.style = "display:grid"
-    divs.style = 'text-align:center'    
+    divs.style = 'grid-template-columns: auto auto auto auto'
+   
 
     products.forEach(product => {
         //defining elements
         const productDiv = document.createElement('div');
         const productImage = document.createElement('img');
-        const productTitle = document.createElement('h3');
-        const productPrize = document.createElement('h4');
+        const productTitle = document.createElement('h5');
+        const productPrize = document.createElement('h3');
 
         //populate properties with data
         productImage.src = product.image;
         productTitle.innerHTML = product.title;
-        productPrize.innerHTML = product.price
+        productPrize.innerHTML = `Price: ${product.price} $`
 
-        console.log(productImage.src );
-
+        console.log(`Price:${product.title}`);
         //append elements to productDiv
         productDiv.appendChild(productTitle);
         productDiv.appendChild(productImage);
@@ -38,7 +37,17 @@ function getData() {
 
         divs.appendChild(productDiv);
 
+        //styling elements
         productImage.style.height="200px"
+        productPrize.style.paddingLeft='30px'
+        productTitle.style.fontSize='15px'
+        productPrize.style.paddingBottom= '100px'
+        productTitle.style.paddingTop= '100px'
+        productTitle.style.wordSpacing ='5px'
+        productTitle.style.wordBreak
+        productPrize.style.color = 'red'
+        productTitle.style.paddingLeft = '20px'
+        productTitle.style.paddingRight = '20px'
 
     });
 }); 
