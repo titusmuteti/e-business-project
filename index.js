0ae7a5678dc64afc98e9eebe35e1442ccf0c6999
Titus Mumo <titusmumo@gmail.com>
2022-08-31 15:26:35 +0300

    add like and dislike buttons

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+document.addEventListener('DOMContentLoaded', getData)
+
 
 const public_api_url = 'https://fakestoreapi.com/products';
-let items = document.querySelector('div#items');
 
 
 
@@ -23,17 +24,26 @@ function getData() {
         const productImage = document.createElement('img');
         const productTitle = document.createElement('h5');
         const productPrize = document.createElement('h3');
+        const likeBtn =  document.createElement('button');
+        const disLikeBtn =  document.createElement('button');
+
+        //number of likes
+        let count = 0;
 
         //populate properties with data
         productImage.src = product.image;
         productTitle.innerHTML = product.title;
         productPrize.innerHTML = `Price: ${product.price} $`
+        likeBtn.innerHTML = `${count} likes`
+        disLikeBtn.innerHTML = `${count} dislikes`
 
         console.log(`Price:${product.title}`);
         //append elements to productDiv
         productDiv.appendChild(productTitle);
         productDiv.appendChild(productImage);
-        productDiv.appendChild(productPrize)
+        productDiv.appendChild(productPrize);
+        productDiv.appendChild(likeBtn);
+        productDiv.appendChild(disLikeBtn);
 
         divs.appendChild(productDiv);
 
@@ -41,13 +51,17 @@ function getData() {
         productImage.style.height="200px"
         productPrize.style.paddingLeft='30px'
         productTitle.style.fontSize='15px'
-        productPrize.style.paddingBottom= '100px'
+        productPrize.style.paddingBottom= '0px'
         productTitle.style.paddingTop= '100px'
         productTitle.style.wordSpacing ='5px'
         productTitle.style.wordBreak
         productPrize.style.color = 'red'
         productTitle.style.paddingLeft = '20px'
         productTitle.style.paddingRight = '20px'
+        disLikeBtn.style.paddingRight = '50px'
+        likeBtn.style.paddingRight = '50px'
+        likeBtn.style.fontSize = '20px'
+        disLikeBtn.style.fontSize = '20px'
 
     });
 }); 
