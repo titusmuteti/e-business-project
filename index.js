fd47bcc00747952d36bb3b487f296ddefe0a0f12
Titus Mumo <titusmumo@gmail.com>
2022-09-01 12:19:25 +0300

    button edits

--- index.js
+++ index.js
@@ -22,10 +22,12 @@ function getData() {
         const productPrize = document.createElement('h3');
         const likeBtn =  document.createElement('button');
         const disLikeBtn =  document.createElement('button');
+        const noOfLikes = document.createElement('span');
+        const noOfDisLikes = document.createElement('span')
 
         //defining likes/dislikes
         let like = 0;
-        let likecount = like + 1;
+        let likeCount = like + 1;
         let dislike = 0;
         let dislikeCount =dislike + 1;
 
@@ -33,48 +35,54 @@ function getData() {
         productImage.src = product.image;
         productTitle.innerHTML = product.title;
         productPrize.innerHTML = `Price: ${product.price} $`
-        likeBtn.innerHTML = `${like} likes`
+        likeBtn.innerHTML = `like`
         disLikeBtn.innerHTML = `${dislike} dislike`
+        noOfLikes.innerHTML = `${like} likes`;
+        noOfDisLikes.innerHTML = `${dislike} likes`
 
         //append elements to productDiv
         productDiv.appendChild(productTitle);
         productDiv.appendChild(productImage);
         productDiv.appendChild(productPrize);
         productDiv.appendChild(likeBtn);
+        productDiv.appendChild(noOfLikes);
         productDiv.appendChild(disLikeBtn);
+        productDiv.appendChild(noOfDisLikes)
+        
 
         divs.appendChild(productDiv);
 
         //styling elements
         productImage.style.height="200px"
+        productImage.style.marginLeft = '10px'
         productPrize.style.paddingLeft='30px'
-        productTitle.style.fontSize='15px'
+        productTitle.style.fontSize='20px'
         productPrize.style.paddingBottom= '0px'
         productTitle.style.paddingTop= '40px'
         productTitle.style.wordSpacing ='5px'
-        productTitle.style.wordBreak
         productPrize.style.color = 'red'
         productTitle.style.paddingLeft = '20px'
         productTitle.style.paddingRight = '20px'
         productPrize.style.paddingBottom = '20px'
-        disLikeBtn.style.paddingRight = '50px'
-        likeBtn.style.paddingRight = '50px'
-        likeBtn.style.fontSize = '20px'
-        disLikeBtn.style.fontSize = '20px'
+        disLikeBtn.style.marginLeft = '30px'
+        likeBtn.style.marginLeft = '10px'
+        likeBtn.style.fontSize = '15px'
+        disLikeBtn.style.fontSize = '15px'
+        noOfLikes.style.marginLeft = '5px'
+        noOfDisLikes.style.marginLeft = '5px'
 
         //adding event listener to like button
         likeBtn.addEventListener('click', ()=> {
-            if (likeBtn.disabled === false) {
-                console.log('true');
-            }
-            likeBtn.innerHTML = `${likecount} likes`
             likeBtn.style.backgroundColor = '#89CFF0'
-            disLikeBtn.disabled = true  
+            likeBtn.textContent = 'liked';
+            noOfLikes.textContent = `${likeCount} Likes`
+            disLikeBtn.disabled = true  ;
         });
         //adding event listener to dislike button
         disLikeBtn.addEventListener('click', ()=> {
-            disLikeBtn.innerHTML = `${dislikeCount} Dislikes`;
-            disLikeBtn.style.backgroundColor = '#c9252c ';
+            disLikeBtn.style.backgroundColor = '#c9252c';
+            likeBtn.textContent = 'Disliked';
+            noOfDisLikes.textContent = `${dislikeCount} Dislikes`
             likeBtn.disabled = true; 
         });
 
